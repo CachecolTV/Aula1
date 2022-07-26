7b02b23a60e7c526a514c5da1d880380947288bb
CachecolTV <97043400+CachecolTV@users.noreply.github.com>
2022-07-26 19:52:54 -0300

    Exercicios aula1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Aula1
-Primeiro repositório!
+# SAtemplate
---
+++ sketch.js
@@ -0,0 +1,5 @@
+
+console.log("hello");
+console.log("Boa noite!!! Meu primeiro exercicio de aula!");
+console.log("Ambiente Local")
+console.log("Servidor Local")
